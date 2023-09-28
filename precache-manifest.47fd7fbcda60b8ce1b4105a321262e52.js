b2262af6e3eb3478b0f6e1663e7536ce13e6bcd6
UpasnaNaithani <upasnanaithani866@gmail.com>
2023-09-28 19:58:33 +0530

    Updates

---
+++ precache-manifest.47fd7fbcda60b8ce1b4105a321262e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "389aa0b07465e7f151fd",
+    "url": "/textutiles-react/static/css/main.26b1f9de.chunk.css"
+  },
+  {
+    "revision": "389aa0b07465e7f151fd",
+    "url": "/textutiles-react/static/js/main.389aa0b0.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutiles-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutiles-react/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "e5e396d22d25aeefab2e",
+    "url": "/textutiles-react/static/js/runtime~main.e5e396d2.js"
+  },
+  {
+    "revision": "607aee2b88a0cee92cd6857668f08b9c",
+    "url": "/textutiles-react/index.html"
+  }
+];
